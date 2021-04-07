70e3aa814eb536a9e40941e65db79d9832046d86
fredrbus <fredrbus@stud.ntnu.no>
2021-04-07 18:23:52 +0200

    Most of b) done

--- PE5/benchmarking.c
+++ PE5/benchmarking.c
@@ -2,25 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <signal.h>
 
-// a. Pipeline functionality (3 points)
-// Write the program using unnamed pipes (system call pipe(2)) and output
-// the cumulative number of received bytes after each read call of
-// the parent process
+// Global variables
+// Used inside SIGALRM-handler
+ssize_t bytes_read_this_alarm = 0;
+ssize_t bytes_read_total = 0;
+int seconds = 0;
+
+void compute_bandwidth(int signum)
+{
+    //printf("Bandwidth:\t %luB/s\n", bytes_read_this_alarm);
+
+    bytes_read_total += bytes_read_this_alarm;
+    bytes_read_this_alarm = 0;
+    alarm(1);
+    seconds++;
+
+    //
+    //  Lines underneath prints mean, used to compare bandwith by block sizes
+    //
+    // if ((seconds % 5) == 0)
+    // {
+    //     long mean = bytes_read_total / seconds;
+    //     printf("Mean:\t\t %lu\n", mean);
+    // }
+}
 
 void read_cummulative(int read_descriptor, int block_size)
 {
-    char read_chars[2048];
+    char *read_chars;
     ssize_t read_bytes = 0;
-    ssize_t cummulative = 0;
+    signal(SIGALRM, compute_bandwidth);
+
+    alarm(1);
     while (1)
     {
         read_bytes = read(read_descriptor, &read_chars, block_size);
         if (read_bytes > 0)
         {
-            cummulative += read_bytes;
+            bytes_read_this_alarm += read_bytes;
             // Task a) Print cummulative bytes
-            printf("Bytes read:\t%lu\n", cummulative);
+            // printf("Bytes read:\t%lu\n", bytes_read_this_alarm);
         }
         else if (read_bytes == 0)
         {
@@ -38,30 +61,28 @@ int main(int argc, char const *argv[])
     // File descriptors used in pipe
     int fd[2]; // fd[0] - read,    fd[1] - write
 
-    int block_size = 1024;
+    // Sets new blocksize - if provided
+    long block_size = 1024;
     if (argc > 1)
     {
-        block_size = atoi(argv[1]);
+        block_size = atol(argv[1]);
     }
-    printf("Block size: %d\n", block_size);
+    printf("Block size: %lu\n", block_size);
 
+    // Create unnamed pipe
     if (pipe(fd) == -1)
     {
         perror("Error opening pipe: ");
     }
 
     int id = fork();
-    ssize_t wrote = 0;
 
     if (id == 0)
     {
         while (1)
         {
-
             write(fd[1], "", block_size);
         }
-
-        //printf("pid:\t%d\t i:\t%d\n", id, i);
     }
     else
     {
